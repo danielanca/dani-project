b796111c391ddb31a33becc663061666ce0d2359
Daniel Anca <steptu94@gmail.com>
2021-06-01 15:40:40 +0300

    Update
    New things added

--- src/components/House_details.js
+++ src/components/House_details.js
@@ -1,6 +1,7 @@
 import Menu_header from './Menu_header';
 import './house_details.css';
-
+import contact_profile_pic from '../media/avatar.png'
+import arrow_right from '../media/arrow.png';
 // Second page
 function House_details()
 {
@@ -22,6 +23,26 @@ function House_details()
           
         </div>
 
+        <div className="contact-form"> 
+          <div className="view-profile">
+            <img className="profilephoto" src={contact_profile_pic}></img> 
+             <div className="profile-text">
+                <p className="contactname">Kayley Hall</p>
+                <p className="profile-link">View profile</p>
+             </div>
+             
+          </div>
+          <div className="form-container">
+              <input className="form" id="firstname" type="text"  placeholder=" First Name " />
+              <input className="form" id="firstname" type="text"  placeholder=" Phone " />
+              <input className="form" id="firstname" type="text"  placeholder=" Email " />
+              <input className="form" id="firstname" type="text"  placeholder=" Hello, I'm interested in " />
+              {/* <label for="firstname" class="form-field__label">First name</label> */}
+          </div>
+          <div className="view-more">
+            <button className="button-viewmore"> View more <img className="arrow-c" src={arrow_right}></img></button>
+          </div>
+        </div>
 
       </div>
       
