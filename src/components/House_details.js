468b423a7c237703c8f651fbd4a956b7bffcbf00
Daniel Anca <steptu94@gmail.com>
2021-06-01 17:26:41 +0300

    Update
    New update

--- src/components/House_details.js
+++ src/components/House_details.js
@@ -2,6 +2,8 @@ import Menu_header from './Menu_header';
 import './house_details.css';
 import contact_profile_pic from '../media/avatar.png'
 import arrow_right from '../media/arrow.png';
+import picArray from '../media/image_importer';
+import image_src from '../media/house_interior.png';
 // Second page
 function House_details()
 {
@@ -22,28 +24,46 @@ function House_details()
           </div>
           
         </div>
+        <div className="big-wrapper">
+            <img className="imagePreviewFull" src={image_src}></img>
+            <div className="preview-container">
+              
+                <img src={}></img>
+            </div>
+            <div className="details-container">
+               <div className="details-title">Details </div>
+               <div className="details-slots">
 
-        <div className="contact-form"> 
-          <div className="view-profile">
-            <img className="profilephoto" src={contact_profile_pic}></img> 
-             <div className="profile-text">
-                <p className="contactname">Kayley Hall</p>
-                <p className="profile-link">View profile</p>
-             </div>
-             
-          </div>
-          <div className="form-container">
-              <input className="form" id="firstname" type="text"  placeholder=" First Name " />
-              <input className="form" id="firstname" type="text"  placeholder=" Phone " />
-              <input className="form" id="firstname" type="text"  placeholder=" Email " />
-              <input className="form" id="firstname" type="text"  placeholder=" Hello, I'm interested in " />
-              {/* <label for="firstname" class="form-field__label">First name</label> */}
-          </div>
-          <div className="view-more">
-            <button className="button-viewmore"> View more <img className="arrow-c" src={arrow_right}></img></button>
-          </div>
-        </div>
+                    <div className="slot-box">  1</div>
+                    <div className="slot-box">  2</div>
+                    <div className="slot-box">  3</div>
+                    <div className="slot-box">  4</div>
+                    <div className="slot-box">  5</div>
 
+               </div>
+            </div>
+       
+            <div className="contact-form"> 
+              <div className="view-profile">
+                <img className="profilephoto" src={contact_profile_pic}></img> 
+                <div className="profile-text">
+                    <p className="contactname">Kayley Hall</p>
+                    <p className="profile-link">View profile</p>
+                </div>
+                
+              </div>
+              <div className="form-container">
+                  <input className="form" id="firstname" type="text"  placeholder=" First Name " />
+                  <input className="form" id="firstname" type="text"  placeholder=" Phone " />
+                  <input className="form" id="firstname" type="text"  placeholder=" Email " />
+                  <input className="form" id="firstname" type="text"  placeholder=" Hello, I'm interested in " />
+                  {/* <label for="firstname" class="form-field__label">First name</label> */}
+              </div>
+              <div className="view-more">
+                <button className="button-viewmore"> View more <img className="arrow-c" src={arrow_right}></img></button>
+              </div>
+            </div>
+          </div>
       </div>
       
 
---
+++ src/components/image_importer.js
@@ -0,0 +1,15 @@
+import house1 from '../../assets/img/accesories/hats/hat1.png';
+import house2 from '../../assets/img/accesories/hats/hat2.png';
+import house3 from '../../assets/img/accesories/hats/hat3.png';
+import house4 from '../../assets/img/accesories/hats/hat4.png';
+import house5 from '../../assets/img/accesories/hats/hat5.png';
+import house6 from '../../assets/img/accesories/hats/hat6.png';
+
+export default [
+    house1,
+    house2,
+    house3,
+    house4,
+    house5,
+    house6
+];
