50f205996a1ee3cc279245aefe4a6f558ef8dadb
Daniel Anca <steptu94@gmail.com>
2021-07-07 11:19:29 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ import House_details from "./page_sections/House_details";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-    let house_id = 1;
-    return (
-        <div className="App">
-            <Router>
-                <Route path="/home" component={HomePage} />
+	let house_id = 1;
+	return (
+		<div className="App">
+			<Router>
+				<Route path="/" component={HomePage} />
 
-                <Route path="/details/:id">
-                    <House_details />
-                </Route>
-            </Router>
-        </div>
-    );
+				<Route path="/details/:id">
+					<House_details />
+				</Route>
+			</Router>
+		</div>
+	);
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
